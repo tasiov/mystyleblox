9a5fbf1d8a65f098440f5d835ade64918e10b3e0
Mario Aguayo <mario@mystyleblox.com>
2015-02-21 15:02:55 -0500

    Added validations to all forms. More validations needed in talent forms

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
+  validates :style, :education, :experience, :clients, :equipment, :rate, :type, presence: true
+  
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
 	before_save {self.email = email.downcase}
 	validates :name, :phone, :email, presence: true
 	validates :name, length: {minimum: 6}
+	validates :name, format: {with: /[a-zA-Z\\s]/, :message => "field is incorrect. Please enter your first and last name."}
+	validates :phone, format: {with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/, :message => "number must be valid. i.e xxx-xxx-xxxx"}
 	validates :email, uniqueness: {case_sensitive: false}
-	validates :email, format: {with: /\A[^@]+@[^@]+\z/}
+	validates :email, format: {with: /\A[^@]+@[^@]+\z/, :message => "must be a valid email address."}
 
 	has_one :client
 	has_one :male
