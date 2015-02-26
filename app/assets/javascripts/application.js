9fe90e89ee59c1534f7a90810628670f2845b4ac
Steven Luengo <stevenluengo@gmail.com>
2015-02-26 14:25:49 -0500

    Fixed bugs on calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,14 +30,16 @@ $(document).ready(function() {
   eventColor: '#ff4351',
       dayClick: function(date) {
         $(this).css('background-color', '#ff4351');
-        var userID = $("#calendar").attr("user-id");
-		var unAvaliable = date.toString()
+        var userInput = $('#calendar').attr('user-id');
+    	var unAvaliable = date.toString()
+    // alert("User ID: " + userID);
         $.post("/unavailables", {
-        unavailable: {
-        date: unAvaliable,
-        user: userID,
-        }
+	        unavailable: {
+	        date: unAvaliable,
+	        user_id: userInput,
+	        }
         })
-    }
-    })
+
+      }
+   })
 });
