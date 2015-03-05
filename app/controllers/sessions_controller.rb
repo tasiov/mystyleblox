250f9e2645d8fa0a683645fdbc3bf8d59f6a7ff6
Steven Luengo <stevenluengo@gmail.com>
2015-03-05 18:15:40 -0500

    Login button now takes you to users profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,22 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      # Log ins to the appropriate users profile page.
+      if !current_user.male.nil?
+        redirect_to user.male
+      elsif !current_user.female.nil?
+        redirect_to user.female
+      elsif !current_user.hair.nil?
+        redirect_to user.hair
+      elsif !current_user.photo.nil?
+        redirect_to user.photo
+      elsif !current_user.mua.nil?
+        redirect_to user.mua
+      elsif !current_user.stylist.nil?
+        redirect_to user.stylist
+      else
+        redirect_to root_url
+      end
     else
       flash.now[:error] = 'Email/Password combination is invalid!'
       render 'new'
@@ -17,4 +32,7 @@ class SessionsController < ApplicationController
   	log_out
   	redirect_to root_url
   end
+
+
+
 end
