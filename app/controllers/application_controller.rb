4fb17f47d165ae26e03f8944c33b2367f4888de1
Steven Luengo <stevenluengo@gmail.com>
2015-03-07 11:22:38 -0500

    added my profile button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+  def current_user_profession(user)
+    if !current_user.male.nil?
+        user.male
+      elsif !current_user.female.nil?
+        user.female
+      elsif !current_user.hair.nil?
+        user.hair
+      elsif !current_user.photo.nil?
+        user.photo
+      elsif !current_user.mua.nil?
+        user.mua
+      elsif !current_user.stylist.nil?
+        user.stylist
+      else
+        root_url
+      end
+  end
 end
+
+
