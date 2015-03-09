f02e59e54a8a02e7b2215f6446eedb6aa96603d9
Kassandra M <kassandram022@gmail.com>
2015-03-09 15:22:29 -0400

    fix search image positioning and adjust slider

---
+++ app/assets/images/img/My Style Blox_files/application-2e745f54b2dad8cfda2f4c497f44635d.js
@@ -0,0 +1,87 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+
+
+
+
+
+
+
+$.ajaxSetup({
+	headers: {
+		'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+	}
+});
+
+fullCal = function(){
+	var currentUser = $('#user-now').attr('user-id');
+	var uid = $('#calendar').attr('data-uid');
+	$('#calendar').fullCalendar({
+		height: 275,
+		eventLimit: true,
+		allDayDefault: true,
+		allDaySlot: true,
+		eventSources:[
+		{
+			url:'/unavailables/'+ uid +'.json',
+			cellColor: '#ff4351',
+		}
+		],
+		eventColor: '#ff4351',
+		dayClick: function(date) {
+			if (uid === currentUser){
+				// var userInput = $('#user-now').attr('user-id');
+				var dateBox = this;
+				var unAvaliable = date.toString()
+				// alert("User ID: " + userID);
+				$.ajax({
+				  type: "POST",
+				  url: "/unavailables",
+				  data: {
+				  	unavailable: {
+							date: unAvaliable,
+							user_id: currentUser,
+						}
+					},
+					beforeSend: function(xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+				  success: function() {
+				  	// This is where you make the box red.
+						$(dateBox).css('background-color', '#ff4351');
+				  },
+				  //dataType: dataType
+				});
+			}
+		}
+});
+}
+
+
+dropdown = function() {
+
+	$('.dropdown-toggle').dropdown()
+}
+
+$(document).ready(fullCal);
+$(document).ready(dropdown);
+$(document).on('page:load', fullCal);
+$(document).on('page:load', dropdown);
+
+
+
+
+
+
+
+
+
