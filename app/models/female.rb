a80e6fbd9dd2d50048f578cda7241160dc2c39f1
Mario Aguayo <mario@mystyleblox.com>
2015-03-10 19:13:54 -0400

    changes to routes

--- app/models/female.rb
+++ app/models/female.rb
@@ -1,5 +1,5 @@
 class Female < ActiveRecord::Base
-  validates :height, :weight, :dress, :shoes
+  validates :height, :weight, :age, :hair_c, :hair_l, :eyes, :bust, :waist, :hips, :dress, :shoes, :skin, :tattoo, :pierce, :scar, :nudity, :experience, :rate, presence: true
   belongs_to :user
 
   after_create :get_zip
